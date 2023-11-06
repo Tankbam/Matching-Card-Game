7498058dded0bc86f63a1870ad11794a50b17477
Tankbam <116445294+Tankbam@users.noreply.github.com>
2023-11-05 20:41:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const gridContainer = document.querySelector(".grid-container");
+const resetButton = document.getElementById("reset-button");
+
+let matched = 0;
+let isFlipping = false;
+
+function resetGame() {
+    gridContainer.innerHTML = "";
+    openedCells = [];
+    matchedPairs = 0;
+    createGrid();
+}
+function createGrid(){
+  const grid = document.getElementById("grid");  
+}
+function startGrid(){
+  
+}
